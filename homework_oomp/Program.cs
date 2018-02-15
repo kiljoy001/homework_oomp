f795114a1a4da4355427a2d762c0c6ec4578a867
kiljoy001 <kiljoy001@gmail.com>
2018-02-14 18:16:51 -0800

    Add project files.

---
+++ homework_oomp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace homework_oomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
