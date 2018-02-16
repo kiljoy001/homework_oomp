f36e8dd7ce87c9261e3b76776f95e1e0eab18a1d
kiljoy001 <kiljoy001@gmail.com>
2018-02-15 23:29:51 -0800

    Created add message unit test

--- MsgTest/UnitTest1.cs
+++ MsgTest/UnitTest1.cs
@@ -1,31 +1,44 @@
-using Autofac;
+using Moq;
 using homework_oomp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Scrypt;
+using System.Collections.Generic;
 
 namespace MsgTest
 {
     [TestClass]
     public class UnitTest1
     {
+        private const string payload = "{\"Id\":\"123\",\"DateOfRegistration\":\"2012-10-21T00:00:00+05:30\",\"Status\":0}";
         [TestMethod]
-        public void Test_Processor()
+        public void Test_Processor_Add_Message()
         {
             //Arrange
-            var Node = new ContainerBuilder();
-            var Wet = new ContainerBuilder();
-            var Time = new ContainerBuilder();
-            Node.RegisterType<NodeMsg>().As<IMessage>();
-            Wet.RegisterType<WetMsg>().As<IMessage>();
-            Time.RegisterType<TimeMsg>().As<IMessage>();
+            var mockNode = new Mock<IMessage>();
+            var mockWet = new Mock<IMessage>();
+            var mockTime = new Mock<IMessage>();
             ScryptEncoder hashstring = new ScryptEncoder();
             string fakeHeader = hashstring.Encode("somerandomstring");
+            mockNode.Setup(n => n.Header()).Returns(fakeHeader);
+            mockNode.Setup(n => n.Payload()).Returns(payload);
+            mockWet.Setup(n => n.Header()).Returns(fakeHeader);
+            mockWet.Setup(n => n.Payload()).Returns(payload);
+            mockTime.Setup(n => n.Header()).Returns(fakeHeader);
+            mockTime.Setup(n => n.Payload()).Returns(payload);
             MsgProcessor processor = new MsgProcessor();
-           
-           
-            //Act
-            
 
+            //Act
+            processor.Add(mockNode.Object);
+            processor.Add(mockWet.Object);
+            processor.Add(mockTime.Object);
+             
+            
+            //Assert
+            Assert.AreEqual(processor.Digest.Count, 3);
+            foreach(IMessage item in processor.Digest)
+            {
+                Assert.IsInstanceOfType(item, typeof(IMessage));
+            }
         }
     }
 }
